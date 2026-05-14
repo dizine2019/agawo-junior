cb0421ac887e188bb7c0f930d0efcb2b633e2d60
Fredrick <agawoprimaryandjunior@gmail.com>
2026-05-14 15:02:00 +0300

    Add admin credentials

--- create_admin.py
+++ create_admin.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 username = 'admin'
-password = 'YourSecurePassword123'  # <-- Change this to your desired password!
+password = 'Psyz2060'  # <-- Change this to your desired password!
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email='', password=password)
